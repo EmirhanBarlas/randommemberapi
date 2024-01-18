e927b49f26b7a1f8953de0c2d4184fb1dd991f71
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-01-18 21:20:43 +0300

    RandomMemberAPI

---
+++ randomizeapi.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from faker import Faker
+import random
+
+app = Flask(__name__)
+
+fake = Faker()
+
+countries = ["USA", "Canada", "UK", "Germany", "France", "Italy", "Spain", "Japan", "China", "Australia"]
+
+@app.route('/random_person', methods=['GET'])
+def get_random_person():
+    country = random.choice(countries)
+    name = fake.first_name()
+    surname = fake.last_name()
+    age = random.randint(18, 99)
+
+    person = {
+        'country': country,
+        'name': name,
+        'surname': surname,
+        'age': age
+    }
+
+    return jsonify(person)
+
+if __name__ == '__main__':
+    app.run(debug=True)
